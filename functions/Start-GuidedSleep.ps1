6a40749f8557e22134b78bceb256ba428cadfd4a
Kieran Jacobsen <kieran.jacobsen@outlook.com>
2015-02-05 14:56:26 +1100

    2 New Functions
    guided sleep and wait for ad replication

---
+++ functions/Start-GuidedSleep.ps1
@@ -0,0 +1,16 @@
+﻿#http://www.ehloworld.com/878
+function Start-GuidedSleep 
+{
+    [cmdletbinding()]
+    param(
+        [parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true, Mandatory = $true, HelpMessage = 'No time specified')]
+        [int]$s
+    )
+    for ($i = 1; $i -lt $s; $i++) 
+    {
+        [int]$TimeLeft = $s-$i
+        Write-Progress -Activity "Waiting $s seconds..." -PercentComplete (100/$s*$i) -CurrentOperation "$TimeLeft seconds left ($i elapsed)" -Status 'Please wait'
+        Start-Sleep -Seconds 1
+    }
+    Write-Progress -Completed -Activity $true -Status 'Please wait'
+}
---
+++ functions/Wait-ADReplication.ps1
@@ -0,0 +1,39 @@
+﻿function wait-ADReplication 
+{
+    [cmdletbinding()]
+    $StartTime = Get-Date
+    $ExpiryTime = $StartTime.AddMinutes($Timeout)
+
+    $Total = (Get-ADReplicationUpToDatenessVectorTable -Target * -Credential $DomainAdminCredentials | Where-Object  -FilterScript {
+            $_.partner -ne $null
+        }
+    ).length
+
+
+    do 
+    {
+        $Now = Get-Date
+        $NumberReplicated = @((Get-ADReplicationUpToDatenessVectorTable -Target * -Credential $DomainAdminCredentials | Where-Object  -FilterScript {
+                    ($_.partner -ne $null) -and ($_.LastReplicationSuccess -gt $StartTime)
+                }
+        )).length
+
+
+        if ($NumberReplicated -lt $Total)
+        {
+            Write-Verbose -Message "$NumberReplicated of $Total have replicated"
+            Write-Progress -Activity 'Waiting for AD Replication' -Status Waiting -PercentComplete (100 * $NumberReplicated/$Total) -SecondsRemaining ($ExpiryTime.Subtract($Now).TotalSeconds)
+            Start-Sleep -Seconds 5
+        }
+    }
+    while (($ExpiryTime -gt $Now) -and ($NumberReplicated -lt $Total))
+
+    Write-Verbose -Message "$NumberReplicated of $Total have replicated"
+
+    if ($NumberReplicated -lt $Total)
+    {
+        Write-Error -Message "Not all domain controllers and zones have replicated. $NumberReplicated of $Total have replicated"
+    }
+
+    Write-Progress -Activity 'Waiting for AD Replication' -Status Waiting -Completed
+}
